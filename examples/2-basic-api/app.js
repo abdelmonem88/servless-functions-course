3d382f5df80c346efca6044f54c3472dacca2f64
abdelmonem88 <abdelmonem1888@gmail.com>
2021-02-18 10:41:36 +0200

    edit

--- examples/2-basic-api/app.js
+++ examples/2-basic-api/app.js
@@ -1,1 +1,16 @@
-const result = document.querySelector('.result')
+const result = document.querySelector(".result");
+
+const fetchData = async () => {
+ const response = await axios.get("/api/2-basic-api");
+ const products = response.data;
+
+ const productsNames = response.data
+  .map((product) => {
+   const { name } = product;
+   return `<p>${name}</p>`;
+  })
+  .join("");
+ result.innerHTML = productsNames;
+};
+
+fetchData();
