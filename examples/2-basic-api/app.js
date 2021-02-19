9ddab360dbf650f0c4831a355f0918b866d94be9
abdelmonem88 <abdelmonem1888@gmail.com>
2021-02-19 03:39:07 +0200

    add air table api

--- examples/2-basic-api/app.js
+++ examples/2-basic-api/app.js
@@ -1,18 +1,22 @@
 const result = document.querySelector(".result");
 
 const fetchData = async () => {
- const response = await axios.get(
-  "https://serverless-functions-tutorial.netlify.app/api/2-basic-api"
- );
- const products = response.data;
+ try {
+  const response = await axios.get(
+   "https://serverless-functions-tutorial.netlify.app/api/2-basic-api"
+  );
+  const products = response.data;
 
- const productsNames = response.data
-  .map((product) => {
-   const { name } = product;
-   return `<p>${name}</p>`;
-  })
-  .join("");
- result.innerHTML = productsNames;
+  const productsNames = response.data
+   .map((product) => {
+    const { name } = product;
+    return `<p>${name}</p>`;
+   })
+   .join("");
+  result.innerHTML = productsNames;
+ } catch (error) {
+  console.log(error);
+ }
 };
 
 fetchData();
